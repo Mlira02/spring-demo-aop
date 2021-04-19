1b66cb8e3fa3a95d5406370e228ebe3a71ee7174
Marcos Lira <liram1995@gmail.com>
2021-04-19 16:00:28 -0500

    Added after and around aspects to this project

--- src/com/marcos/aopdemo/MainDemoApp.java
+++ src/com/marcos/aopdemo/MainDemoApp.java
@@ -2,31 +2,46 @@ package com.marcos.aopdemo;
 
 import com.marcos.aopdemo.dao.AccountDAO;
 import com.marcos.aopdemo.dao.MembershipDAO;
+import com.marcos.aopdemo.service.TrafficFortuneService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.lang.reflect.Member;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class MainDemoApp
 {
+
+    private static Logger myLogger = Logger.getLogger(MainDemoApp.class.getName());
+
     public static void main(String[] args)
     {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(DemoConfig.class);
 
-        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
-        List<Account> theAccounts = null;
-        try
-        {
-            boolean tripWire = true;
-            theAccounts = theAccountDAO.findAccounts(tripWire);
-        }
-        catch(Exception exc)
-        {
-            System.out.println("Main program caught exception: " + exc);
-        }
-        System.out.println("Main Program: DemoApp ");
-        System.out.println(theAccounts);
+        TrafficFortuneService theFortuneService = context.getBean("trafficFortuneService", TrafficFortuneService.class);
+
+//        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+//        List<Account> theAccounts = null;
+//        try
+//        {
+//            boolean tripWire = true;
+//            theAccounts = theAccountDAO.findAccounts(tripWire);
+//        }
+//        catch(Exception exc)
+//        {
+//            myLogger.info("Main program caught exception: " + exc);
+//        }
+//        myLogger.info("Main Program: DemoApp ");
+//        myLogger.info(theAccounts);
+        myLogger.info("Main program DemoApp");
+
+        boolean tripWire = true;
+        String data = theFortuneService.getFortune(tripWire);
+
+        myLogger.info("My fortune is: " + data);
+
+        myLogger.info("finished");
 
         context.close();
     }
--- src/com/marcos/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/marcos/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,15 +2,14 @@ package com.marcos.aopdemo.aspect;
 
 import com.marcos.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 
 @Aspect
@@ -18,26 +17,27 @@ import java.util.List;
 @Order(2)
 public class MyDemoLoggingAspect
 {
+    private Logger myLogger = Logger.getLogger(getClass().getName());
 //    @Before("execution(public void add*())") could use this but creating pointcut can be used. duh...
     @Before("com.marcos.aopdemo.aspect.AOPExpressions.forDAOPackageNoGetterSetter()")
     public void beforeAddAccountAdvice(JoinPoint theJoinPoint)
     {
-        System.out.println("======== Executing @Before advice on all methods with add in front ========");
+        myLogger.info("======== Executing @Before advice on all methods with add in front ========");
 
         MethodSignature methodSig = (MethodSignature)theJoinPoint.getSignature();
-        System.out.println("Method: " + methodSig);
+        myLogger.info("Method: " + methodSig);
 
         Object[] args = theJoinPoint.getArgs();
 
         for (Object tempArg : args)
         {
-            System.out.println("This is tempArg: " + tempArg);
+            myLogger.info("This is tempArg: " + tempArg);
 
             if(tempArg instanceof Account)
             {
                 Account theAccount = (Account)tempArg;
-                System.out.println("Account name: " + theAccount.getName());
-                System.out.println("Account level: " + theAccount.getLevel());
+                myLogger.info("Account name: " + theAccount.getName());
+                myLogger.info("Account level: " + theAccount.getLevel());
             }
         }
     }
@@ -47,11 +47,11 @@ public class MyDemoLoggingAspect
     public void AfterReturningFindAccountsAdvice(JoinPoint theJoinPoint, List<Account> result)
     {
         String method = theJoinPoint.getSignature().toShortString();
-        System.out.println("====== Executing afterReturning on method: " + method + "========");
-        System.out.println("Result is: " + result);
+        myLogger.info("====== Executing afterReturning on method: " + method + "========");
+        myLogger.info("Result is: " + result);
 
         convertAccountNamesToUpperCase(result);
-        System.out.println("====== results have changed as is: " + result + " =========");
+        myLogger.info("====== results have changed as is: " + result + " =========");
     }
 
     public void convertAccountNamesToUpperCase(List<Account> result)
@@ -63,4 +63,41 @@ public class MyDemoLoggingAspect
         }
     }
 
+    @AfterThrowing(pointcut = "execution(* com.marcos.aopdemo.dao.AccountDAO.findAccounts(..))",
+                   throwing = "theExc")
+    public void AfterThrowingFindAccountsAdvice(JoinPoint theJoinPoint, Throwable theExc)
+    {
+        String method = theJoinPoint.getSignature().toShortString();
+        myLogger.info("======= Executing @AfterThrowing on method: " + method);
+        myLogger.info("===== the exception is: " + theExc);
+    }
+
+    @After("execution(* com.marcos.aopdemo.dao.AccountDAO.findAccounts(..))")
+    public void AfterFinallyFindAccounts(JoinPoint theJoinPoint)
+    {
+        String method = theJoinPoint.getSignature().toShortString();
+        myLogger.info("======= Executing after(finally) on method: " + method);
+    }
+
+    @Around("execution(* com.marcos.aopdemo.service.TrafficFortuneService.getFortune(..))")
+    public Object AroundGetFortune(ProceedingJoinPoint theProceedingJoinPoint) throws Throwable
+    {
+        String method = theProceedingJoinPoint.getSignature().toShortString();
+        myLogger.info("===== Executing @Around aspect on method: " + method + " ========");
+
+        long begin = System.currentTimeMillis();
+        Object result = null;
+        try{
+            result = theProceedingJoinPoint.proceed();
+        }
+        catch(Exception exc)
+        {
+            myLogger.warning(exc.getMessage());
+            result = "Major accident! No worries this will be resolved soon!";
+        }
+        long end = System.currentTimeMillis();
+        long duration = end - begin;
+        myLogger.info("Duration of method: " + duration / 1000.0 + " seconds");
+        return result;
+    }
 }
